99723af98caa2e0b6c40266700d067f8fbcef609
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-06-01 14:25:00 +0530

    Create 11switchWeekday.java

---
+++ 11switchWeekday.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class week{
+public static void main(String[]args){
+  Scanner s=new Scanner(System.in);
+  int weekday;
+  System.out.println("enter number from (1 to 7):");
+  weekday=s.nextInt();
+  switch(weekday){
+    case 1:
+    System.out.println("monday");
+    break;
+    case 2:
+    System.out.println("tuesday");
+    break;
+    case 3:
+    System.out.println("wednesday");
+    break;
+    case 4:
+    System.out.println("thursday");
+    break;
+    case 5:
+    System.out.println("friday");
+    break;
+    case 6:
+    System.out.println("saturday");
+    break;
+    case 7:
+    System.out.println("sunday");
+    break;
+    default:
+    System.out.println("invalid number");
+  }
+}
+}
